1d95cfd375e8499a4cc3f0d1618097596f87b137
alexa <48258700+Litteeen@users.noreply.github.com>
2021-01-24 15:46:41 +0100

    initial commit

---
+++ Tweak/Ballet.h
@@ -0,0 +1,23 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+BOOL enabled = NO;
+
+UIImageView* wallpaperView;
+AVQueuePlayer* player;
+AVPlayerItem* playerItem;
+AVPlayerLooper* playerLooper;
+AVPlayerLayer* playerLayer;
+
+BOOL useImageWallpaperSwitch = NO;
+BOOL useVideoWallpaperSwitch = NO;
+
+@interface HBAppGridViewController : UIViewController
+@end
+
+@interface HBUIMainAppGridTopShelfContainerView : UIView
+@end
+
+@interface HBBadgeOverlayView : UIView
+@end
